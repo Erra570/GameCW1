1ac5319c0431a562d15e6f3e02969b5624dc56d6
Voidhi <l.lakomicki@gmail.com>
2025-02-26 21:21:26 +0000

    rules panel

---
+++ Assets/Scripts/ClosePanel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosePanel : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject player;
+
+    public void Close()    {
+        Debug.Log("Closing. . .");
+        player.GetComponent<ThirdViewMvt>().enabled = true; // as it capture the mouse
+        panel.SetActive(false); 
+    }
+}
